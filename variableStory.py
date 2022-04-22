ea3d93ff2c76d5a9c5e932d52419079cfab6a61e
nigam <nigam@NIGAMESH>
2022-04-22 16:38:52 +0530

    Daily Coding- Part 1

---
+++ variableStory.py
@@ -0,0 +1,8 @@
+char1_name = "Manav"
+char2_name = "Catherine"
+
+year = "2015"
+
+print(char1_name + " loves " + char2_name)
+print("They met in the year " + year + ".")
+print("They parted their ways in 2017 and never ever met again till 2022.")
