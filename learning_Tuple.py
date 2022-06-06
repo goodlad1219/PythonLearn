6f9e51a1eb09a9e138ff3f72d67e1268bd2d3566
nigam <nigam@NIGAMESH>
2022-06-06 10:03:59 +0530

    Tuples

---
+++ learning_Tuple.py
@@ -0,0 +1,10 @@
+# Creating a Tuple
+coordinates = (4,5)
+
+# Accessing individual element in a tuple
+print(coordinates[0])
+print(coordinates[1])
+
+# Tuples are generally immutable
+# coordinates[0] = 10
+# throws error: TypeError: 'tuple' object does not support item assignment
