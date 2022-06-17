8d3d247b6c48ed264ab37d1eaaddb016e7d24c3d
nigam <nigam@NIGAMESH>
2022-06-17 17:30:43 +0530

    Hackerrank problems

---
+++ HackerRank/problem1.py
@@ -0,0 +1,14 @@
+"""
+https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+"""
+def main():
+    
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    main()
+
