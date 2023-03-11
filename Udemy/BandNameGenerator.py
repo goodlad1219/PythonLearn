4ac6f4c9324c828cc1deef6050ecd0218cf3bd5f
nigam <nigam@NIGAMESH>
2023-03-11 20:42:17 +0530

    Day 1 rearranged

--- Udemy/BandNameGenerator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the band name generator")
-city = input("What's the name of the city you grew in?\n")
-pet_name = input("What's your pet name?\n")
-print("Your band name could be "+city+" "+pet_name)
