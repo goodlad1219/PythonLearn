7152f1c10a008ae8a1f6ef4c14e0243ea9466a02
nigam <nigam@NIGAMESH>
2023-10-18 02:08:54 +0530

    Basic pomodoro timer

--- MajorProject/Timer.py
+++ MajorProject/Timer.py
@@ -1,3 +1,27 @@
 import time
 import os
 
+def convert(t):
+    return t * 60
+
+def countdown(t, label):
+    # 60
+    while t:
+        min,sec = divmod(t, 60)
+        print(f"{label}:{min:02d}:{sec:02d}",end="\r")
+        time.sleep(1)
+        t -= 1
+
+def pomodoro(work, rest):
+    #convert min to sec
+    w = convert(work)
+    r = convert(rest)
+    countdown(w, "Work")
+    os.system("cls")
+    countdown(r, "Rest")
+    os.system("cls")
+
+work = int(input("Work time (min): "))
+rest = int(input("Rest time (min): "))
+1
+pomodoro(work, rest)
