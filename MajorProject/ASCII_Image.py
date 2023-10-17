1c7571ad359ba8da839857bc8cc8241109f2d69a
nigam <nigam@NIGAMESH>
2023-10-18 00:58:16 +0530

    updates

--- MajorProject/ASCII_Image.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-window = tk.Tk()
-window.title('ASCII Image')
-resize = tk.Canvas(window,width=720, height=880)
-resize.pack()
-window.configure(bg='black')
-window.mainloop()
-
